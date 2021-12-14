ee0be8da19de40cddbfe7031f4321a04493fa314
OmarElKhatibCS <omarelkhatib@mail.com>
2021-12-14 07:29:52 +0200

    add ui utils

---
+++ app/ui/utils/colors.go
@@ -0,0 +1,51 @@
+package utils
+
+import "github.com/gdamore/tcell/v2"
+
+const (
+	MainPageGridTitleColor   = tcell.ColorOrange
+	MainPageGridBorderColor  = tcell.ColorLightGrey
+	MainPageTableTitleColor  = tcell.ColorLightSkyBlue
+	MainPageTableBorderColor = tcell.ColorGrey
+
+	LoggedMainPageTitleColor     = tcell.ColorLightGoldenrodYellow
+	LoggedMainPagePubStatusColor = tcell.ColorSandyBrown
+
+	GuestMainPageTitleColor = tcell.ColorOrange
+	GuestMainPageDescColor  = tcell.ColorLightGrey
+	GuestMainPageTagColor   = tcell.ColorLightSteelBlue
+)
+
+const (
+	AnimePageGridTitleColor   = tcell.ColorOrange
+	AnimePageGridBorderColor  = tcell.ColorLightGrey
+	AnimePageTableTitleColor  = tcell.ColorLightSkyBlue
+	AnimePageTableBorderColor = tcell.ColorGrey
+
+	AnimePageInfoViewTitleColor  = tcell.ColorLightSkyBlue
+	AnimePageInfoViewBorderColor = tcell.ColorLightGrey
+
+	AnimePageChapNumColor      = tcell.ColorLightYellow
+	AnimePageTitleColor        = tcell.ColorLightSkyBlue
+	AnimePageDownloadStatColor = tcell.ColorPowderBlue
+	AnimePageReadStatColor     = tcell.ColorOrange
+	AnimePageScanGroupColor    = tcell.ColorDarkSalmon
+	AnimePageHighlightColor    = tcell.ColorMediumSpringGreen
+)
+
+const (
+	SearchPageGridTitleColor   = tcell.ColorOrange
+	SearchPageGridBorderColor  = tcell.ColorLightGrey
+	SearchPageTableTitleColor  = tcell.ColorLightSkyBlue
+	SearchPageTableBorderColor = tcell.ColorGrey
+
+	SearchFormLabelColor = tcell.ColorWhite
+)
+
+const (
+	HelpPageBorderColor = tcell.ColorLightGrey
+)
+
+const (
+	ModalColor = tcell.ColorDarkSlateGrey
+)
---
+++ app/ui/utils/ids.go
@@ -0,0 +1,14 @@
+package utils
+
+const (
+	MainPageID   = "main_page"
+	MangaPageID  = "manga_page"
+	HelpPageID   = "help_page"
+	SearchPageID = "search_page"
+
+	DownloadModalID         = "download_modal"
+	DownloadFinishedModalID = "download_error_modal"
+	ToggleWatchAnimeModalID = "toggle_watch_anime_modal"
+	GenericAPIErrorModalID  = "api_error_modal"
+	OffsetErrorModalID      = "offset_error_modal"
+)
