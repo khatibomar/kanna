cb4fa006cb7d9c27fc69a03a5fedc7f0166b72a1
OmarElKhatibCS <omarelkhatib@mail.com>
2021-12-14 07:10:43 +0200

    add app service

--- app/service/service.go
+++ app/service/service.go
@@ -1,4 +1,31 @@
-package main
+package service
+
+import (
+	"log"
+
+	"github.com/khatibomar/angoslayer"
+	"github.com/khatibomar/tkanna/app/core"
+	"github.com/rivo/tview"
+)
 
 func Start() {
+	cfg := angoslayer.NewConfig(core.App.Config.ClientID, core.App.Config.ClientSecret)
+	core.App = &core.Tkanna{
+		Client:     angoslayer.NewAngoClient(cfg),
+		TView:      tview.NewApplication(),
+		PageHolder: tview.NewPages(),
+	}
+
+	// ui.ShowMainPage()
+	log.Println("Initialised starting screen.")
+	// ui.SetUniversalHandlers()
+
+	log.Println("Running app...")
+	if err := core.App.TView.Run(); err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func Shutdown() {
+	core.App.Shutdown()
 }
--- main.go
+++ main.go
@@ -1,1 +1,8 @@
 package main
+
+import "github.com/khatibomar/tkanna/app/service"
+
+func main() {
+	service.Start()
+	defer service.Shutdown()
+}
